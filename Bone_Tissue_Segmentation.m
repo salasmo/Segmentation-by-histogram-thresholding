348bee4e6854f2889bbf7d95a4df5152bf1f9988
salasmo <92708064+salasmo@users.noreply.github.com>
2025-03-09 17:08:50 -0600

    Bone_Tissue_Segmentation.m

--- Bone_Tissue_Segmentation.m
+++ Bone_Tissue_Segmentation.m
@@ -3,7 +3,7 @@ clear;
 close all;
 
 % Folder containing DICOM files
-dicomFolder = 'C:\Users\santi\OneDrive\Documentos\MATLAB 2025\DU01_knee_06mm';
+dicomFolder = ''; %Add the path of the dataset
 
 % Read all DICOM files from the folder
 dicomFiles = dir(fullfile(dicomFolder, '*.dcm'));
